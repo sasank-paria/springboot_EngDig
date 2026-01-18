eb6cd750ccf8091cf2dcaab9e8c9cd442ac07bc5
paria <83511327+sasank-paria@users.noreply.github.com>
2026-01-18 14:03:11 +0530

    initial

---
+++ myFirstProject/src/main/java/com/digest/myFirstProject/MyClass.java
@@ -0,0 +1,19 @@
+package com.digest.myFirstProject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+    @GetMapping("hi")
+    public String sayHi(){
+        return "hiii";
+    }
+}
